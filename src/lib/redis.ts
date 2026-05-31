6789d61b3749347ae05b7109a552ce2d27d1780d
Shakerullah <sourovmdshakerullah@gmail.com>
2026-05-31 18:25:51 +0600

    fix: fix redis configuration

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-export const ioredis = new Redis({ host: "redis", port: 6379 });
+export const ioredis = new Redis(process.env.REDIS_URL || "redis://redis:6379");
 
 export const redis = {
   async set<T>(
