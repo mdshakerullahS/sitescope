29ddde5d1c461de96abc7db35716ddd8cfb5413d
Shakerullah <sourovmdshakerullah@gmail.com>
2026-05-31 17:16:53 +0600

    feat: install and configure redis using docker compose

---
+++ src/lib/redis.ts
@@ -0,0 +1,27 @@
+import Redis from "ioredis";
+
+export const ioredis = new Redis({ host: "redis", port: 6379 });
+
+export const redis = {
+  async set<T>(
+    key: string,
+    value: T,
+    exSeconds: number = 300,
+  ): Promise<T | "OK" | null | void> {
+    try {
+      const stringValue = JSON.stringify(value);
+      return await ioredis!.set(key, stringValue, "EX", exSeconds);
+    } catch (error) {
+      console.error("Redis SET error:", error);
+    }
+  },
+
+  async get<T>(key: string): Promise<T | null | void> {
+    try {
+      const data = await ioredis!.get(key);
+      return data ? JSON.parse(data) : null;
+    } catch (error) {
+      console.error("Redis GET error:", error);
+    }
+  },
+};
